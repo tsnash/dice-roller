a51d0e7052d0c5f668314a9d1873b7d9a7d26b0c
tsnash <tsnash@users.noreply.github.com>
2025-08-28 23:02:54 -0400

    renamed test to reflect behavior

--- test/dice_roller_test.dart
+++ test/dice_roller_test.dart
@@ -3,7 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:dice_roller/dice_roller.dart';
 
 void main() {
-  test('adds one to input values', () {
+  test('roll returns 6', () {
     expect(roll(), 6);
   });
 }
