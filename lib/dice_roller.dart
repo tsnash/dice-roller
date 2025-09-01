4623bc4f3d6e018147a046fd82e2fe502c062ba9
tsnash <tsnash@users.noreply.github.com>
2025-09-01 17:02:21 -0400

    addressed nitpicks

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,31 +1,54 @@
+import 'dart:math';
+
 import 'package:dice_roller/roll_result.dart';
 export 'roll_result.dart';
 
+/// A class that simulates rolling dice.
+///
+/// The [DiceRoller] class is mutable, allowing you to change the number of dice
+/// and sides on the dice.
 class DiceRoller {
-  var _diceCount = 1;
-  var _sides = 6;
+  int _diceCount = 1;
+  int _sides = 6;
+  Random _rng = Random();
 
-  DiceRoller setDiceCount(int count) {
-    _diceCount = count;
+  /// Seeds the random number generator.
+  ///
+  /// Calling this method will reset the internal state of the random number generator,
+  /// so subsequent calls to [roll] will produce the same sequence of numbers
+  /// for a given seed.
+  DiceRoller seed(int seed) {
+    _rng = Random(seed);
     return this;
   }
 
-  DiceRoller setSides(int sides) {
+  /// Sets the number of dice to roll.
+  ///
+  /// Throws an [ArgumentError] if [diceCount] is less than 1.
+  DiceRoller withDiceCount(int diceCount) {
+    if (diceCount < 1) {
+      throw ArgumentError.value(
+          diceCount, 'diceCount', 'there must be at least 1 die');
+    }
+    _diceCount = diceCount;
+    return this;
+  }
+
+  /// Sets the number of sides on each die.
+  ///
+  /// Throws an [ArgumentError] if [sides] is less than 2.
+  DiceRoller withTotalSides(int sides) {
+    if (sides < 2) {
+      throw ArgumentError.value(
+          sides, 'sides', 'each die must have at least 2 sides');
+    }
     _sides = sides;
     return this;
   }
 
+  /// Rolls the dice and returns a [RollResult].
   RollResult roll() {
-    if (_diceCount <= 0) {
-      throw ArgumentError.value(
-          _diceCount, 'count', 'must be at least 1 die');
-    }
-    if (_sides <= 0) {
-      throw ArgumentError.value(
-          _sides, 'sides', 'die must have at least 1 side');
-    }
-    // Stubbed result; replace with RNG in a follow-up.
-    final rolls = List.generate(_diceCount, (_) => 1);
+    final rolls = List.generate(_diceCount, (_) => _rng.nextInt(_sides) + 1);
     return RollResult.unmodifiable(rolls);
   }
 }
--- test/dice_roller_test.dart
+++ test/dice_roller_test.dart
@@ -19,20 +19,40 @@ void main() {
 
     test('can change the number of dice to roll', () {
       final diceRoller = DiceRoller();
-      diceRoller.setDiceCount(2);
+      diceRoller.withDiceCount(2);
       expect(diceRoller.roll().rolls, hasLength(2));
     });
 
     test('can change the number of sides on a die', () {
       final diceRoller = DiceRoller();
-      diceRoller.setSides(4);
-      expect(diceRoller.roll().rolls, hasLength(1));
+      expect(diceRoller.withTotalSides(4), same(diceRoller));
+      final rolls = diceRoller.roll().rolls;
+      expect(rolls, hasLength(1));
+      expect(rolls, everyElement(inInclusiveRange(1, 4)));
     });
 
     test('can chain methods', () {
       final diceRoller = DiceRoller();
-      diceRoller.setDiceCount(3).setSides(10);
-      expect(diceRoller.roll().rolls, hasLength(3));
+      final chained = diceRoller.withDiceCount(3).withTotalSides(10);
+      expect(identical(chained, diceRoller), isTrue);
+      final result = diceRoller.roll().rolls;
+      expect((result), hasLength(3));
+      expect(result, everyElement(inInclusiveRange(1, 10)));
+    });
+
+    test('invalid inputs throw', () {
+      expect(() => DiceRoller().withDiceCount(0), throwsArgumentError);
+      expect(() => DiceRoller().withTotalSides(1), throwsArgumentError);
+    });
+
+    test('boundary values are accepted', () {
+      expect(() => DiceRoller().withDiceCount(1), returnsNormally);
+      expect(() => DiceRoller().withTotalSides(2), returnsNormally);
+    });
+
+    test('negative values throw', () {
+      expect(() => DiceRoller().withDiceCount(-1), throwsArgumentError);
+      expect(() => DiceRoller().withTotalSides(-1), throwsArgumentError);
     });
   });
 
