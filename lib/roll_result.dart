a89649d9e10ea32ec92cb8eb54342454222c2f7e
tsnash <tsnash@users.noreply.github.com>
2025-09-02 18:28:50 -0400

    addressed nitpcks

--- lib/roll_result.dart
+++ lib/roll_result.dart
@@ -3,6 +3,9 @@ import 'package:flutter/foundation.dart';
 
 @immutable
 class RollResult {
+  final List<int> _rolls;
+  late final int _totalValue = _rolls.fold(0, (a, b) => a + b);
+
   RollResult(List<int> rolls) : _rolls = List.unmodifiable(rolls);
 
   RollResult.unmodifiable(List<int> rolls) : _rolls = List.unmodifiable(rolls);
@@ -10,11 +13,8 @@ class RollResult {
   factory RollResult.constant(List<int> rolls) =>
       RollResult.unmodifiable(rolls);
 
-  final List<int> _rolls;
-
-  List<int> get values => _rolls;
-
-  int get totalValue => _rolls.fold(0, (a, b) => a + b);
+  List<int> get values => UnmodifiableListView(_rolls);
+  int get totalValue => _totalValue;
 
   @override
   int get hashCode => const ListEquality<int>().hash(_rolls);
@@ -26,5 +26,5 @@ class RollResult {
           const ListEquality<int>().equals(_rolls, other._rolls);
 
   @override
-  String toString() => 'RollResult(rolls: $_rolls, total: $totalValue)';
+  String toString() => 'RollResult(values: $_rolls, totalValue: $totalValue)';
 }
